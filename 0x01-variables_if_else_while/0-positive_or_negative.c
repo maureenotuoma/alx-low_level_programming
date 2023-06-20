b32c32a4e9853e81c5bc7f3f049751fe74844515
maureenotuoma <maureenotuoma3@gmail.com>
2023-06-20 03:17:05 -0700

    Task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <time.h>
+/**
+ * main -Program to generate random numbers to detect -ve or +ve
+ *
+ * Return: 0 (success)
+ *
+ */
+int main(void)
+{
+	int n;
+
+	strand(time(0);
+
+	n = rand() - RAND_MAX / 2;
+if (n > 0)
+	{
+printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+	printf("%d is zero\n", n);
+	}
+	else
+	{
+	print("%d is negative\n", n);
+	}
+	return (0);
+	}
