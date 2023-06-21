d476000140b8e8b8543e7ac57b2f2f24399400de
maureenotuoma <maureenotuoma3@gmail.com>
2023-06-21 03:24:21 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
 #include "main.h"
+
+
 /**
- *
- * main: Entry point
- * description: 'prints _putcharto stdout'
- * Return: 0 (success)
+ *main -prints _putchar and a new line 
+ *Return 0 (success)
  */
+
 int main(void)
+
 {
-	char str[] = "_putchar";
-	int i;
-
-	for (i = 0; i < '^' ^ 7; i++);
-	{
-_putchar(str[i]);
-	}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
-
 return (0);
 }
