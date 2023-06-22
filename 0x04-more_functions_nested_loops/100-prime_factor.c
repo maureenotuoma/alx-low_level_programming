1381d44e4a2da47d98d79fb52583d9f24163c0d5
maureenotuoma <maureenotuoma3@gmail.com>
2023-06-22 15:02:30 -0700

    Task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 unsigned long int i, n = 612852475143;
-for (i = 3; i < 782849; i = i + 1 + 2)
+for (i = 3; i < 782849; i = 1 + 2)
 {
 	while ((n % i == 0) && (n != i))
 		n = n / i;
