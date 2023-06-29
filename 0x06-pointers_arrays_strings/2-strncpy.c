644d47b3306f9c40ac8097af4ae572443e234a0e
maureenotuoma <maureenotuoma3@gmail.com>
2023-06-29 04:38:40 -0700

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncpy - copy a string
+ * @dest: Input value
+ * @src: Input value
+ * @n: Input value
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int j = 0;
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[j] = src[j];
+j++;
+}
+while (j < n)
+{
+dest[j] = '\0';
+j++;
+}
+return (dest);
+}
